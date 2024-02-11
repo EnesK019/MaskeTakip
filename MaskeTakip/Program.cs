bbe30f093bf4daf1ac68e57fad6b7f729d492bcf
EnesK019 <eneskrtnc.19@hotmail.com>
2024-02-11 20:07:14 +0300

    Add project files.

---
+++ MaskeTakip/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person();
+person1.FirstName = "Enes";
+person1.LastName = "Kartancı";
+person1.DateOfBirthYear = 2002;
+person1.NationalIdentity = 12345678910;
+
+PttManager pttmanager = new PttManager(new PersonManager());
+pttmanager.GiveMask(person1);
